8a528d518f331f27575451e8f0023059a61cae65
c0wie <krowickijanek559@gmail.com>
2024-06-20 09:03:19 +0200

    add rule of 5 to transform object

--- phy/Transform.hpp
+++ phy/Transform.hpp
@@ -12,7 +12,42 @@ namespace phy
     public:
         Transform() :
             Position(Vector2(0.0f, 0.0f)), Scale(Vector2(0.0f, 0.0f)) {};
+
         Transform(const Vector2 &position, const Vector2 &scale) :
             Position(position), Scale(scale) {};
+
+        Transform(const Transform &other) :
+            Position(other.Position), Scale(other.Scale) {};
+
+        Transform(Transform &&other) :
+            Position(other.Position), Scale(other.Scale)
+        {
+            other.Position = Vector2(0.0f, 0.0f);
+            other.Scale = Vector2(0.0f, 0.0f);
+        };
+
+        Transform& operator=(const Transform &other)
+        {
+            if(this == &other)
+            {
+                return *this;
+            }
+            Position = other.Position;
+            Scale = other.Scale;
+            return *this;
+        }
+
+        Transform& operator=(Transform &&other)
+        {
+            if(this == &other)
+            {
+                return *this;
+            }
+            Position = other.Position;
+            Scale = other.Scale;
+            other.Position = Vector2(0.0f, 0.0f);
+            other.Scale = Vector2(0.0f, 0.0f);
+            return *this;
+        }
     };
 }
