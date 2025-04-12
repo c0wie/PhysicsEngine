f6a00ec63dc4ad11b399c5eb43a44ed83c63be59
root <root@DESKTOP-6KMCL72>
2025-04-12 21:29:57 +0200

    feat! change CollisionWorld's objects container from vector to unordered_map Users don't need to have easy access to objects pointers and removing objects is not so frequent.

--- pe2d/include/collision_world.hpp
+++ pe2d/include/collision_world.hpp
@@ -8,14 +8,10 @@
 
 //std
 #include <functional>
-#include <unordered_map>
+#include <vector>
 
 namespace pe2d {
 class CollisionWorld {
-  using iterator = std::unordered_map<size_t, CollisionBody *>::iterator;
-  using const_iterator =
-      std::unordered_map<size_t, CollisionBody *>::const_iterator;
-
 public:
   CollisionWorld() = default;
   explicit CollisionWorld(int substeps) : m_Substeps(substeps) {
@@ -31,46 +27,31 @@ public:
   CollisionWorld &operator=(CollisionWorld &&other) = default;
 
 public:
-  void Step(float delta_time);
-  void AddObject(CollisionBody *object, std::size_t id);
-  iterator RemoveObject(size_t id);
-  iterator RemoveObject(iterator object) { return m_Objects.erase(object); }
-  iterator RemoveObjects(iterator first_object, iterator last_object) {
-    return m_Objects.erase(first_object, last_object);
-  }
-  void ClearObjects() { m_Objects.clear(); }
-
+  void ResolveCollisions(float delta_time);
+  void AddCollisionObject(CollisionBody *object);
+  void RemoveCollisionObject(CollisionBody *object);
+  
   void SetSolver(std::function<void(std::vector<Collision> &collisions)> solver) {
     m_Solver = solver;
   }
-
+  
   void AddGrid(Vec2f top_left_corner, int rows, int columns, float cell_size);
   void RemoveGrid();
-  void ResizeGrid(Vec2f top_left_corner, int rows, int columns,
-                  float cell_size);
-
+  
   bool IsGridOn() const { return m_IsGridOn; }
   size_t Size() const { return m_Objects.size(); }
   bool Empty() const { return m_Objects.empty(); }
-  iterator Begin() { return m_Objects.begin(); }
-  iterator End() { return m_Objects.end(); }
-  const_iterator cBegin() const { return m_Objects.cbegin(); }
-  const_iterator cEnd() const { return m_Objects.cend(); }
-  CollisionBody * At(unsigned int id);
+  void Clear() { m_Objects.clear(); }
 
 private:
-  void ResolveCollisions(float delta_time);
-  void FindCollisions(size_t idA, size_t idB,
+  void FindCollisions(CollisionBody *a, CollisionBody *b,
                       std::vector<Collision> &collisions);
-  void ApplyGravity();
-  void MoveObjects(float delta_time);
-
 private:
+  bool m_IsGridOn{false};
+  unsigned int m_Substeps{1U};
   Grid m_Grid;
   std::function<void(std::vector<Collision> &collisions)>
       m_Solver{PositionSolver};
-  unsigned int m_Substeps{1U};
-  bool m_IsGridOn{false};
-  std::unordered_map<size_t, CollisionBody *> m_Objects;
+  std::vector<CollisionBody *> m_Objects;
 };
 } //namespace pe2d
